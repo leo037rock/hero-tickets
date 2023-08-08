d2c7677dfb3f2a60fbcde9b36a84b06ea0d1e166
leo037rock <leo037rock@gmail.com>
2023-08-08 15:59:11 -0300

    add error middleware

--- backend/src/infra/database.ts
+++ backend/src/infra/database.ts
@@ -5,7 +5,7 @@ export async function connect(){
         await mongoose.connect(
             'mongodb+srv://alleece:KHzo0Mxt302N1NIY@cluster0.6enfikq.mongodb.net/hero-tickets',
             );
-            console.log('Connect database sucess');
+            console.log('Connect database success');
     } catch (error) {
         console.log(" ~ file: database.ts:5 ~ connect ~ error:", error)
     }
