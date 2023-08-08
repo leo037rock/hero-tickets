ab609a539a3fa60d6e1fd22b2d6f1ba88d4e2131
leo037rock <leo037rock@gmail.com>
2023-08-08 14:18:38 -0300

    First commit

---
+++ backend/src/infra/database.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export async function connect(){
+    try {
+        await mongoose.connect(
+            'mongodb+srv://alleece:KHzo0Mxt302N1NIY@cluster0.6enfikq.mongodb.net/hero-tickets',
+            );
+            console.log('Connect database sucess');
+    } catch (error) {
+        console.log(" ~ file: database.ts:5 ~ connect ~ error:", error)
+    }
+}
